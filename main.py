5354e9b7bf6553329cd586883a4a8fdb88236092
SiennaB124 <04310613@nebraska.edu>
2026-04-09 23:02:44 +0000

    completed lab 10

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sienna Bonner
+#Date: 4/7/26
+#Assignment: Lab 10
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("reaction_time_data.csv")
+
+plt.plot(data["Trial"], data["ReactionTime_ms"], marker='o')
+
+plt.xlabel("Trial")
+plt.ylabel("Reaction Time (ms)")
+plt.title("Reaction Time Across Trials")
+
+plt.savefig("reaction_time_graph.png")
+
